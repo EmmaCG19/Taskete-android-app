a79fb2bd3f6033557654438f5cb56052ec4c6e00
EmmanuelF <emmanuelfernandez600@gmail.com>
2020-11-14 23:44:32 -0300

    Comments and logs were deleted

--- app/src/main/java/com/example/taskete/AboutMeActivity.kt
+++ app/src/main/java/com/example/taskete/AboutMeActivity.kt
@@ -1,11 +1,27 @@
 package com.example.taskete
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
 
 class AboutMeActivity : AppCompatActivity() {
+    private lateinit var btnLinkRepo: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_me)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        btnLinkRepo = findViewById(R.id.iconRepo)
+        btnLinkRepo.setOnClickListener {
+            val uri = Uri.parse("http://github.com/EmmaCG19")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/taskete/notifications/TaskReminderReceiver.kt
+++ app/src/main/java/com/example/taskete/notifications/TaskReminderReceiver.kt
@@ -14,7 +14,6 @@ const val DEFAULT_ID = 1000
 
 class TaskReminderReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        Log.d("TaskReminderReceiver", "Esperando que llege algo por aca")
         val bundle = intent?.extras
         val notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val notification = bundle?.getParcelable<Notification>(TASK_NOTIFICATION)
